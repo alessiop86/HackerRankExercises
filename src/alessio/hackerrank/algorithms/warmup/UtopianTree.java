32b81b54b98dc9129d4fd4e7f36ed6748b2e3ceb
alessiop <alessio.piergiacomi@gmail.com>
2015-05-10 17:20:45 +0200

    fix package search

--- src/alessio/hackerrank/algorithms/warmup/UtopianTree.java
+++ src/alessio/hackerrank/algorithms/warmup/UtopianTree.java
@@ -6,26 +6,26 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/utopian-tree
  */
 public class UtopianTree {
-	   public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        int n =  sc.nextInt();
-	        for (int t=0;t<n;t++) {
-	            int cycles = sc.nextInt();
-	            System.out.println(calc(cycles));
-	        }
-	        sc.close();
-	    }
-	    
-	    private static int calc(int x) {
-	        if (x == 0)
-	            return 1;
-	        else {
-	            int calcX_1 = calc(x-1);
-	            if (x % 2 == 0)
-	                return 1 + calcX_1;
-	            else
-	                return 2*calcX_1;
-	        }
-	    }
-	    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int t = 0; t < n; t++) {
+			int cycles = sc.nextInt();
+			System.out.println(calc(cycles));
+		}
+		sc.close();
+	}
+
+	private static int calc(int x) {
+		if (x == 0)
+			return 1;
+		else {
+			int calcX_1 = calc(x - 1);
+			if (x % 2 == 0)
+				return 1 + calcX_1;
+			else
+				return 2 * calcX_1;
+		}
+	}
+
 }
